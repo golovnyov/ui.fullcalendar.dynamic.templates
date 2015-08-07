2b9535152dc8a96392c962d450edd6d5806a1d6b
golovnyov <viacheslav.golovnyov@avologic.com>
2015-08-07 15:13:54 +0300

    Create new project

---
+++ FullCalendar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FullCalendar.Startup))]
+namespace FullCalendar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
